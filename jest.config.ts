4d69453c05cfdc5a61e3f0e6d49986c750b0ef37
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-11-21 22:31:33 +0530

    test: remove unused Jest setup file from configuration

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/tests/jest.setup.ts'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   verbose: true,
 };
