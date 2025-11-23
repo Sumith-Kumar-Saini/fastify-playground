8b92a378fb5669bf435b825d361d125c8525ca0b
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-11-23 14:13:11 +0530

    chore(app): add temporary logging for environment variables

--- src/app.ts
+++ src/app.ts
@@ -15,6 +15,9 @@ export function buildApp(overrides: { mongoUri?: string; logger?: boolean } = { 
     },
   });
 
+  // temporary Line to check failure
+  fastify.log.info({ env: process.env }, "Env variables");
+
   // Ping route
   fastify.get('/ping', () => 'pong\n');
 
